0ab4281360f8034b6ee9e11b54e35b25fab0af17
Alexiacrypte <makayaalexia2019@gmail.com>
2024-11-29 10:25:18 +0100

    commit t

---
+++ src/main/java/fr/isep/Alexia/personne.java
@@ -0,0 +1,23 @@
+package fr.isep.Alexia;
+abstract class Personne{
+       protected String nom;
+                        int identifiant; int adresse; int contact;
+       protected Personne( String nom, int identifiant, int contact, int adresse) {
+                                    this.nom = nom;
+                                    this.identifiant = identifiant;
+                                    this.adresse = adresse;
+                                    this.contact = contact;
+                                    }
+
+       protected String getNom()  {  return nom;}
+       protected int Adresse()  {  return adresse;}
+       protected int Identifiant()  {  return identifiant;}
+       protected int Contact () {  return contact; }
+
+       protected void setNom(String nom) {this.nom = nom;    };
+       protected void setAdresse(int adresse)  {this.adresse = adresse;};
+       protected void setIdentifiant(int identifiant)  {int.identifiant = identifiant;};
+       protected void setContact(int contact)   {int.contact = contact;};
+
+
+       abstract void Afficher();
