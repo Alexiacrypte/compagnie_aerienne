b3015ca6748cd33aa6b6cd8b5356d0a96de1a869
Alexiacrypte <makayaalexia2019@gmail.com>
2024-12-02 00:09:40 +0100

    ne commit

---
+++ src/main/java/fr/isep/Alexia/Reservation.java
@@ -0,0 +1,13 @@
+package fr.isep.Alexia;
+
+public class Reservation extends Passager{
+    private int numeroReservation,
+    int dateReservation, String statut
+    public class Reservation{
+        int numeroReservation, int dateReservation,
+        int identifiant, int adresse, int contact,
+        String passeport, String nom, String statut
+    }
+    super(numeroReservation, dateReservation, nom, identifiant,
+    adresse, contact, passeport, statut )
+}
