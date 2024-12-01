307b8b9c69859caea961c1cd72ec0690592df9da
Alexiacrypte <makayaalexia2019@gmail.com>
2024-12-01 22:22:55 +0100

    new commit

---
+++ src/main/java/fr/isep/Alexia/PersonnelCabine.java
@@ -0,0 +1,23 @@
+package fr.isep.Alexia;
+
+public class PersonnelCabine extends Employe {
+    private String qualification;
+    public class PersonnelCabine{
+           String qualification, String nom,
+        int identifiant, int adresse, int contact
+    }
+    super String qualification, String nom,
+    int identifiant, int adresse, int contact
+}
+public String qualification getstringqualification(){
+    return stringqualification
+}
+public void Setstringqualification
+        (Stringqualifiction Stringqualification){
+    this.Stringqualification = stringqualification;
+}
+void Afficher() {
+    System.out.println();
+    System.out.println();
+    System.out.println();
+}
---
+++ src/main/java/fr/isep/Alexia/Pilote.java
@@ -0,0 +1,23 @@
+package fr.isep.Alexia;
+
+public class Pilote extends Employe {
+    private String LicenceheuresDeVol;
+    public class Pilote {
+         String LicenceheuresDeVol , String nom,
+        int identifiant, int adresse, int contact
+    }
+    super int adresse, int contact, int identifiant,
+    String nom, StringLicenceheuresDeVol
+}
+public StringLicenceheuresDeVol getStringLicenceheuresDeVol(){
+    return StringLicenceheuresDeVol
+}
+public void SetStringLicenceheuresDeVol
+        ( StringLicenceheuresDeVol StringLicenceheuresDeVol){
+    this.StringLicenceheuresDeVol = StringLicenceheuresDeVol;
+}
+void Afficher(){
+    System.out.println();
+    System.out.println();
+    System.out.println();
+}
