530bff8f23e3a81bc34c539f8866234f8037d10f
Alexiacrypte <makayaalexia2019@gmail.com>
2024-12-01 12:04:20 +0100

    nouveau committere

---
+++ src/main/java/fr/isep/Alexia/passager.java
@@ -0,0 +1,23 @@
+package fr.isep.Alexia;
+
+public class Passager extends Personne  {
+    private String passseport;
+
+    public  class Passeport {
+        String nom, Stringpasseport, int identifiant, int adresse, int contact, ;
+    }
+    super (nom, identifiant, adresse, contact);
+    this.Stringpasseport = string passeport;
+
+}
+public StringPasseport getStringPasseport(){
+    return stringPasseport;
+}
+public void setStringPasseport(StringPasseport stringPasseport) {
+    this.stringPasseport = stringPasseport;
+}
+void Afficher(){
+    System.out.println();
+    System.out.println();
+    System.out.println();
+}
