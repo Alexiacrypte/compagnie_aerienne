42f8b370c9d11be601e3f9f979c95d2689edbc50
Alexiacrypte <makayaalexia2019@gmail.com>
2024-12-04 11:43:33 +0100

    new committere

--- src/main/java/fr/isep/Alexia/Aeroport.java
+++ src/main/java/fr/isep/Alexia/Aeroport.java
@@ -1,9 +1,9 @@
 package fr.isep.Alexia;
 
 public class Aeroport extends Vol{
-    String nom, StringVille, Stringdescription, int identifiant,
+    String nom, StringVille, Stringdescriptioon, int identifiant,
     int adresse, int contact, String nom, String statut,
-    String Etat, String passeport, int NumeroVol, String
+    String Etat, String passeport, int Numerovol, String
 }
     super String nom, String ville, String description, int identifiant,
     int adresse, int contact, String passeport, String Etat;
